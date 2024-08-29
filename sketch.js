d89bf6120c593b78c3ede005e9e89d8b5571c87f
Tânia Iinuma <127634555+TaniaIinuma@users.noreply.github.com>
2024-08-29 10:10:38 -0300

    js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+let cor;
+let posicaoHorizontal; //x
+let posicaoVertical; //y
+
 function setup() {
+
     createCanvas(400, 400);
-  }
+    background(color(100, 0, 0));
+    cor = "color(random(0, 255), random(0, 255), random(0,255))";
+    posicaoHorizontal = 200;
+    posicaoVertical = 200;
+    }
+
+function draw() {
+     fill(cor);
+    circle(posicaoHorizontal, posicaoVertical, 50);
   
-  function draw() {
-    background(220);
-  }
+
+    if(mouseX < posicaoHorizontal) {
+    posicaoHorizontal--;
+    }
+    if(mouseX > posicaoHorizontal) {
+    posicaoHorizontal++;
+    }
+if(mouseY < posicaoVertical) {
+    posicaoVertical--;
+}
+
+if(mouseY > posicaoVertical) {
+    posicaoVertical++;
+}
+    if(mouseIsPressed) {
+    cor = color(random(0, 255), random(0, 255), random(0,255),
+random(0, 100));
+    }
+}
+
